e4d4fd75ae9f8548b5d4dfdfa7c88f23669528bb
eragoh <kacpergrau@gmail.com>
2021-12-10 23:04:04 +0100

    //to do now distribution seems to work fine, time to decide if it will be 2+1 or 2+2

--- main.py
+++ main.py
@@ -2,31 +2,62 @@
 
 import random
 import timeit
+import sys
 
-NUMBER_OF_RECORDS = 1000000 #number of students
-BYTES_TO_TAKE = 2048 # 2kB
+NUMBER_OF_RECORDS = 12 #number of students
+BYTES_TO_TAKE = 200 # normally 4kB=4096
+NUMBER_OF_DISK_SAVES = 0
+NUMBER_OF_DISK_READS = -1
 GROWING = True
 
-def get_some_records_from_file(file):
-    rest = '' # rest from the records, to be added on the start of the next run
-    with open(file, "r") as f:
+def get_some_records():
+    with open("records.txt", "r") as f:
         records = f.read(BYTES_TO_TAKE)
         while records:
-            if rest != '':
-                records = rest + records
-                rest = ''
-            while records[-1] != ';':
-                rest += records[-1]
-                records = records[:-1] # delete last char
-                if records == '':
-                    break
-            rest = rest[::-1] # reverse string
-            list = records.split(';') if records else []
-            yield list
+            yield records
             records = f.read(BYTES_TO_TAKE)
 
-def get_record_value():
-    pass # get record value
+def get_record():
+    global NUMBER_OF_DISK_READS
+    gen = get_some_records()
+    rest = '' # rest from the records, to be added on the start of the next run
+    while True:
+        try:
+            NUMBER_OF_DISK_READS += 1
+            records_from_generator = next(gen)
+            if rest != '':
+                records_from_generator = rest + records_from_generator
+                rest = ''
+            while records_from_generator[-1] != ';':
+                rest += records_from_generator[-1]
+                records_from_generator = records_from_generator[:-1] # delete last char
+                if records_from_generator == '':
+                    break
+            rest = rest[::-1] # reverse string
+            list = records_from_generator.split(';')
+            for r in list:
+                yield r
+        except:
+            return
+
+
+# def get_some_records_from_file(file):
+#     rest = '' # rest from the records, to be added on the start of the next run
+#     with open(file, "r") as f:
+#         records = f.read(BYTES_TO_TAKE)
+#         while records:
+#             if rest != '':
+#                 records = rest + records
+#                 rest = ''
+#             while records[-1] != ';':
+#                 rest += records[-1]
+#                 records = records[:-1] # delete last char
+#                 if records == '':
+#                     break
+#             rest = rest[::-1] # reverse string
+#             list = records.split(';') if records else []
+#             yield list
+#             records = f.read(BYTES_TO_TAKE)
 
 def write_records_to_file(file_name, records_list):
     if records_list:
@@ -34,43 +65,75 @@ def write_records_to_file(file_name, records_list):
             file.write(';'.join(records_list)+';')
 
 def draw_grade():
-    return random.choice(['2.0', '2.5', '3.0', '3.5', '4.0', '4.5', '5.0'])
+    return random.choice(['2.0', '3.0', '3.5', '4.0', '4.5', '5.0'])
 
-def create_record():
-    file.write(str(NUMBER_OF_RECORDS)+','+draw_grade()+','+draw_grade()+','+draw_grade()+';')
+def create_record_to_file():
+    file.write(str(NUMBER_OF_RECORDS)+'/'+draw_grade()+draw_grade()+draw_grade()+';')
 
-def get_average_grade():
-    return 1
+def get_record_value(record):
+    i = record.find('/')
+    value = (int)(record[i+1]) + (int)(record[i+3])/10 + (int)(record[i+4])\
+            + (int)(record[i+6])/10 + (int)(record[i+7]) + (int)(record[i+9])/10
+    return value
 
-file = open("records.txt", "w") #change later to binary files .code() and .decode()
-while NUMBER_OF_RECORDS > 0:
-    create_record()
-    NUMBER_OF_RECORDS -= 1
-file.close()
-#records generated, time to sort
+def add_record_to_tape(tape, record):
+    global NUMBER_OF_DISK_SAVES
+    tape.append(record)
+    if sys.getsizeof(tape1 + tape2 + tape3 + tape4) >= BYTES_TO_TAKE * 0.97:
+        NUMBER_OF_DISK_SAVES += 1
+        if tapes_swapped:
+            write_records_to_file(tapes[0], tape2)
+            write_records_to_file(tapes[1], tape1)
+            tape1.clear()
+            tape2.clear()
+        else:
+            write_records_to_file(tapes[0], tape1)
+            write_records_to_file(tapes[1], tape2)
+            tape1.clear()
+            tape2.clear()
+
+
+# change later to binary files .code() and .decode()
+with open("records.txt", "w") as file:
+    while NUMBER_OF_RECORDS > 0:
+        create_record_to_file()
+        NUMBER_OF_RECORDS -= 1
+
+# records generated, time to sort
 tapes = ["tape1.txt", "tape2.txt", "tape3.txt", "tape4.txt"]
-#clear tapes
+# clear tapes
 for t in tapes:
     with open(t, "w") as file:
         pass
 
 start = timeit.default_timer()
 tape1, tape2, tape3, tape4 = [], [], [], []
-which_tape = 1
-for records in get_some_records_from_file("records.txt"):
-    for record in records:
-        if record != '':
-            if which_tape == 1:
-                tape1.append(record)
-                which_tape = 2
-            else:
-                tape2.append(record)
-                which_tape = 1
-    write_records_to_file(tapes[0], tape1)
-    write_records_to_file(tapes[1], tape2)
-    tape1, tape2 = [], []
+tapes_swapped = False
+lval = -1 # last record value
+
+# SORTING
+
+for record in get_record(): # can be done also using next(gen)
+    if record != '':
+        # INITIAL SORT - disposition from main tape to two tapes
+        # count value of record
+        val = get_record_value(record)
+        # compare with last record in the tape
+        # if greater or equal, write to the same tape
+        # otherwise, write to another tape
+        if val >= lval:
+            add_record_to_tape(tape1, record)
+        else:
+            tape2.append(record)
+            add_record_to_tape(tape2, record)
+            tape1, tape2 = tape2, tape1 # swap tape1 and tape2
+            tapes_swapped = not tapes_swapped
+        lval = val
+
 stop = timeit.default_timer()
 print(f'Time::::::{stop-start}')
+print(f'Disk Saves::::::{NUMBER_OF_DISK_SAVES}')
+print(f'Disk Reads::::::{NUMBER_OF_DISK_READS}')
 
 #get 2kB to generator and split it to create ready records
 #care for last record, since it probably won't be complete
